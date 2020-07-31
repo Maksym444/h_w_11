f8278e8d44298fe7aeaeae9e496960e0e95df41b
Maksym <makskryvenko@gmail.com>
2020-07-31 16:21:22 +0300

    first attempt h_w_11

--- src/index.js
+++ src/index.js
@@ -6,11 +6,7 @@ function info (bindArguments) {
   console.log (bindArguments, this.answer)
 }
 
-function bind(context, bindArguments, fn)  {
-	return function () {
-		return fn.apply(context, [bindArguments]);
-	}
-}
+let bind = (context, bindArguments, fn) => () => fn.apply(context, [bindArguments]);
 var bindAsMethod = bind (global, "Окончательный вопрос жизни, Вселенной и всего такого!?", info)
 
 bindAsMethod ()
